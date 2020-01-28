9df09f9e37f1af04396bb42d0c29b2760b04f1d9
GaussianEntrail <53263900+GaussianEntrail@users.noreply.github.com>
2020-01-28 17:19:52 +0500

    Update EntitiesAndSuch.cs

--- EntitiesAndSuch.cs
+++ EntitiesAndSuch.cs
@@ -43,6 +43,7 @@ namespace ConsoleThing
     class Critter
     {
         Point p;
+        Point p_prev;
         Tile t;
         int actiontimer;
         int life, attack, defend, speed;
@@ -58,6 +59,7 @@ namespace ConsoleThing
     class Player : Critter
     {
         Point p;
+        Point p_prev;
         Tile t = Tile.PLAYER;
         int actiontimer;
         int life, attack, defend, speed;
@@ -66,26 +68,29 @@ namespace ConsoleThing
         public Player(int x, int y)
         {
             p = new Point(x, y);
+            p_prev = p;
             CharInit();
         }
         public Player(Point p)
         {
             this.p = p;
+            p_prev = p;
             CharInit();
         }
         public void CharInit()
         {
             life = 4;
-            attack = 6;
-            defend = 5;
-            speed = 9;
+            attack = 10;
+            defend = 10;
+            speed = 10;
             money = 0;
             name = "Explorer";
         }
         public void draw() { t.draw(); }
         public Point getPosition() { return p; }
         public bool isAt(int x, int y) { return (x == p.x && y == p.y); }
-        public void move(int x, int y) { p.move(x, y); }
+        public bool wasAt(int x, int y) { return (x == p_prev.x && y == p_prev.y); }
+        public void move(int x, int y) { p_prev = p; p.move(x, y); }
         public void doTick() { actiontimer += speed; }
         public void doAction() { actiontimer -= 100; }
         public bool isReady() { return (actiontimer >= 100); }
@@ -116,7 +121,7 @@ namespace ConsoleThing
             Console.WriteLine();
 
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write("♦ SPD: {0}", speed);
+            Console.Write("♦ AGI: {0}", speed);
             Console.WriteLine();
 
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -127,6 +132,7 @@ namespace ConsoleThing
     class Monster : Critter
     {
         Point p;
+        Point p_prev;
         Tile t = Tile.MONSTER;
         int actiontimer;
         int life, attack, defend, speed;
@@ -134,25 +140,28 @@ namespace ConsoleThing
         public Monster(int x, int y)
         {
             p = new Point(x, y);
+            p_prev = p;
             CharInit();
         }
         public Monster(Point p)
         {
             this.p = p;
+            p_prev = p;
             CharInit();
         }
         public void CharInit()
         {
-            life = 4;
-            attack = 4;
-            defend = 4;
+            life = 3;
+            attack = 8;
+            defend = 8;
             speed = 7;
             name = "Beast";
         }
         public void draw() { t.draw(); }
         public Point getPosition() { return p; }
         public bool isAt(int x, int y) { return (x == p.x && y == p.y); }
-        public void move(int x, int y) { p.move(x, y); }
+        public bool wasAt(int x, int y) { return (x == p_prev.x && y == p_prev.y); }
+        public void move(int x, int y) { p_prev = p; p.move(x, y); }
         public void doTick() { actiontimer += speed; }
         public void doAction() { actiontimer -= 100; }
         public bool isReady() { return (actiontimer >= 100); }
