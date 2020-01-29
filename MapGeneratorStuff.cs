d452d728b687d26635289a5583dd95495b900fd2
GaussianEntrail <53263900+GaussianEntrail@users.noreply.github.com>
2020-01-29 14:46:00 +0500

    Update MapGeneratorStuff.cs

--- MapGeneratorStuff.cs
+++ MapGeneratorStuff.cs
@@ -27,5 +27,115 @@ namespace ConsoleThing
             }
             return map;
         }
+
+        public static Tile[,] testMapGenerator(int w, int h, Random r, int maxRooms)
+        {
+            Tile[,] map = new Tile[h, w];
+            int minRoomWidth = 3, maxRoomWidth = w / 3;
+            int minRoomHeight = 3, maxRoomHeight = h / 3;
+
+            int x, y;
+            for (y = 0; y < h; y++)
+            {
+                for (x = 0; x < w; x++)
+                {
+                    map[y, x] = Tile.VOID;
+                }
+            }
+
+            List<Rect> rooms = new List<Rect>();
+            while (rooms.Count < maxRooms) {
+                int r_w = randomRange(r, minRoomWidth, maxRoomWidth);
+                int r_h = randomRange(r, minRoomHeight, maxRoomHeight);
+                int r_x = randomRange(r, 1, (w - 1) - r_w);
+                int r_y = randomRange(r, 1, (h - 1) - r_h);
+                rooms.Add(new Rect(r_x, r_y, r_w, r_h));
+            }
+
+            List<Rect> halls = new List<Rect>();
+            for (int n = 0; n < rooms.Count; n++)
+            {
+                int next_n = (n + 1) % rooms.Count;
+                int start_x = rooms[n].CENTER().x;
+                int start_y = rooms[n].CENTER().y;
+                int end_x = rooms[next_n].CENTER().x;
+                int end_y = rooms[next_n].CENTER().y;
+
+                int hall_start_x = Math.Min(start_x, end_x);
+                int hall_end_x = Math.Max(start_x, end_x);
+
+                int hall_start_y = Math.Min(start_y, end_y);
+                int hall_end_y = Math.Max(start_y, end_y);
+
+                int hall_w = hall_end_x - hall_start_x;
+                int hall_h = hall_end_y - hall_start_y;
+
+                if (r.NextDouble() > 0.5)
+                {
+                    halls.Add(new Rect(hall_start_x,hall_start_y,hall_w,1)); //horizontal
+                    halls.Add(new Rect(hall_end_x, hall_start_y, 1, hall_h)); //vertical
+                }
+                else
+                {
+                    halls.Add(new Rect(hall_start_x, hall_start_y, 1, hall_h)); //vertical
+                    halls.Add(new Rect(hall_start_x, hall_end_y, hall_w, 1)); //horizonal
+                }
+            }
+
+            foreach (Rect room in rooms)
+            {
+                int rx, ry;
+                for (ry = room.y; ry < room.y + room.h; ry++)
+                {
+                    for (rx = room.x; rx < room.x + room.w; rx++)
+                    {
+                        map[ry, rx] = Tile.FLOOR;
+                    }
+                }
+            }
+            foreach (Rect hall in halls)
+            {
+                int hx, hy;
+                for (hy = hall.y; hy < hall.y + hall.h; hy++)
+                {
+                    for (hx = hall.x; hx < hall.x + hall.w; hx++)
+                    {
+                        map[hy, hx] = Tile.FLOOR;
+                    }
+                }
+            }
+
+            int i, j;
+            bool adjFloorTile = false;
+            for (y = 0; y < h; y++)
+            {
+                for (x = 0; x < w; x++)
+                {
+                    adjFloorTile = false;
+                    if (map[y,x] == Tile.VOID)
+                    {
+                        for (i = -1; i < 2; i ++)
+                        {
+                            for (j = -1; j < 2; j++)
+                            {
+                                if (x + i > 0 && x + i < w && y + j > 0 && y + j < h)
+                                {
+                                    if (map[y + j, x + i] == Tile.FLOOR) { adjFloorTile = true; break;}
+                                }
+                            }
+                        }
+                        if (adjFloorTile) { map[y, x] = Tile.ROCK; }
+                    }
+                }
+            }
+
+            return map;
+        }
+
+        public static int randomRange(Random r, int min, int max)
+        {
+            int range = max - min;
+            return (min + r.Next(range));
+        }
     }
 }
